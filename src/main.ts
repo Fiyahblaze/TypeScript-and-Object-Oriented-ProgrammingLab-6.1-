ef0b3ed4146af97c6b1d94a8de79ad9dd8cbece4
Justin <woodsj40@gmail.com>
2026-08-11 12:05:13 -0400

    third commit

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,23 @@ export class Product {
         return this.price;
     }
 }
+import { Product } from "./Product";
+
+export class PhysicalProduct extends Product {
+    constructor(
+        sku: string,
+        name: string,
+        price: number,
+        public weight: number
+    ) {
+        super(sku, name, price);
+    }
+
+    override getPriceWithTax(): number {
+        return this.price * 1.10;
+    }
+
+    get formattedWeight(): string {
+        return `${this.weight} kg`;
+    }
+}
